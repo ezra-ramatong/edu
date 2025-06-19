11daaf7b9db16c8eaa2a254b48ecc061343df560
Ezra Ramatong <128799204+ezra-ramatong@users.noreply.github.com>
2025-06-19 18:47:27 +0200

    sumAllTails

--- learn-go-with-tests/04-arrays-slices/sum.go
+++ learn-go-with-tests/04-arrays-slices/sum.go
@@ -7,3 +7,22 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		tails := numbers[1:]
+		sums = append(sums, Sum(tails))
+	}
+	return sums
+}
